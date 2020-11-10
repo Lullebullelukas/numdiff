ece9deb2a4ee2a0492f2a968d658a41de36acaa9
Lukas Gustavsson <43780349+Lullebullelukas@users.noreply.github.com>
2020-11-10 10:55:18 +0100

    first commit

---
+++ errtimeVS.m
@@ -0,0 +1,10 @@
+function errtimeVS(A,y0,t0,tf,N)
+ [~,err] = eulerint(A,y0,t0,tf,N);
+ h =(tf-t0)/N;
+ timearray(1) = 0;
+  for i = 1:N-1
+    timearray(i+1) = timearray(i)+ h;
+  end
+ semilogy(timearray,err);
+ hold on;
+end
