db1d582dec2d9f0e38f203d957f81564d16d8c99
Lukas Gustavsson <43780349+Lullebullelukas@users.noreply.github.com>
2020-11-23 14:28:41 +0100

    Delete RK4step.m

--- RK4step.m
+++
@@ -1,7 +0,0 @@
-function unew = RK4step(f, told, uold, h)
-  Y1 = f(told,uold);
-  Y2 = f(told + h/2,uold+h*Y1/2);
-  Y3 = f(told + h/2,uold+h*Y2/2);
-  Y4 = f(told + h,uold+h*Y3);
-  unew = uold + (h/6)*(Y1 + 2*Y2 + 2*Y3 + Y4);
-end
